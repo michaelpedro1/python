3d6878b910e1250d6ab4978fbb9c6740cc6ceac4
michaelpedro1 <klaw.tweaking@gmail.com>
2025-01-03 12:06:48 +0200

    message

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+print("hello my name is michael pedro")
+a=10
+b=50
+sum= a+b
+print (sum) #sum is a varible 
+#10+50=60
+name=input('what is your name')
+print('your name is ', name )
+
+person1='michael pedro'
+#person2,3,4,5
+date1='01-20-2025'
+#date2,3,4,5
+print('my name is', person1,'i was born in',date1)
