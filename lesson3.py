fbfa904ff0a089c83d35d9c04bc4215ec264dbe1
michaelpedro1 <klaw.tweaking@gmail.com>
2025-01-07 12:48:36 +0200

    message

---
+++ lesson3.py
@@ -0,0 +1,12 @@
+name='michael pedro'
+x=10
+y=40
+print('the type of x is',type(x))
+print('the type of y is',type(y))
+number1=int(input('enter the first number')) 
+number2=int(input('enter the second number'))
+addition=number1 + number2
+print('the edition of number1 and number 2 ', addition)
+fullname=input('enter your name')
+#convert to uppercase
+print(fullname.upper())
---
+++ lesson4.py
@@ -0,0 +1,7 @@
+name1=str(input('enter the first name')) 
+name2=str(input('enter the second name'))
+addition=name1 + name2
+print('the edition of name 1 and name 2 ', addition)
+fullname=input('enter your name')
+#convert to uppercase
+print(fullname.upper())
