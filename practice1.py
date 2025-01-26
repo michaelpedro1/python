340fcd58f71d06f522d82444fa9ead21c55610a7
michaelpedro1 <klaw.tweaking@gmail.com>
2025-01-26 20:19:32 +0200

    message

---
+++ practice1.py
@@ -0,0 +1,32 @@
+v=2
+w=6
+x=8
+y=4
+z=(v+w)*x/y
+
+print(z)
+
+name='michael'
+age=14
+if name == 'michael' or name== 'pedro' and age >=10:
+    print('me')
+else:
+    print('not me')    
+
+num1=int(input('give the first number'))
+num2=int(input('give the second number')) 
+if  num1% num2 ==0:
+    print('divisible')
+else:
+    print('not divisible')      
+
+mim1=38
+total=40
+a=36
+c=56
+sum=mim1*total
+print(sum)
+sum2=sum-(a-c)
+print(sum2)
+mim2=sum2/total
+print('the new average is ',mim2)
