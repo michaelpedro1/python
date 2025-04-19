aef1ad0d71db1b6014fc7d329ce9b04b1526e987
michaelpedro1 <klaw.tweaking@gmail.com>
2025-04-19 16:38:13 +0200

    tuples.py

---
+++ tuples.py
@@ -0,0 +1,13 @@
+tuples1=('studets', 345,5.167,False)
+print(tuples1)
+
+
+
+tuples2=(4,566,64,78,345)
+print(tuples2)
+
+print(len(tuples2))
+
+print('the index value is', tuples2[1])
+
+print('index num 0 to 4',tuples2[2:4])
