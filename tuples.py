110997c8cc62898bb52827959ca1ac9826d0a91a
michaelpedro1 <klaw.tweaking@gmail.com>
2025-04-22 16:32:18 +0200

    tuples.py

--- tuples.py
+++ tuples.py
@@ -11,3 +11,10 @@ print(len(tuples2))
 print('the index value is', tuples2[1])
 
 print('index num 0 to 4',tuples2[2:4])
+
+
+numbers = (2, 3, 4, 5, 6) 
+product = 1
+for num in numbers:
+    product *= num
+print('product of the tuple:', product)    
